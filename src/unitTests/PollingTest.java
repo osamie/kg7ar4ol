e0d56dec1ed3630cadc92a85af76795a9bb48c03
Osazuwa Omigie <osamie2002@gmail.com>
2013-02-07 22:46:40 -0500

    updated polling tests

--- src/unitTests/PollingTest.java
+++ src/unitTests/PollingTest.java
@@ -2,6 +2,8 @@ package unitTests;
 
 import static org.junit.Assert.*;
 
+import java.net.SocketException;
+
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,8 +17,10 @@ import client.Client;
 public class PollingTest {
 	private int polls[] = new int[10];
 	int numOfPolls;
+	
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@After
@@ -28,7 +32,129 @@ public class PollingTest {
 		int testCount = 1;
 		assertTrue(connectionTest(testCount));
 	}
-	
+	@Test
+	public final void testVotes()
+	{
+		int testCount = 5;
+		assertTrue(voteTest(testCount));
+	}
+	@Test
+	public final void testVoterConnections()
+	{
+		int testCount = 5;
+		assertTrue(voteConnectionTest(testCount));
+	}
+	@Test
+	public final void testAdminMessages()
+	{
+		
+		assertTrue(adminMessages());
+	}
+	@Test
+	public final void testMultiplePolls()
+	{
+		int testCount = 5;
+		assertTrue(multiplePolls(testCount));
+	}
+	/**
+	 *Test creating multiple polls.   
+	 * @param numberOfPolls
+	 * @return false if polls fail to be created
+	 */
+	public boolean multiplePolls(int numberOfPolls)
+	{
+		Boolean testResult = true;
+		AdminClient admin = new AdminClient(PollServer.ADMIN_PORT);
+		Client testClient = new Client(PollServer.VOTING_PORT);
+		int pollIds[] = new int[numberOfPolls];
+		
+		//Creates polls and verifies if the polls created pollIds
+		for(int i = 0; i < numberOfPolls; i++)
+		{
+			pollIds[i] = admin.createPoll("4", "1@asd.ca");
+			if(pollIds[i] == 0) testResult = false;
+		}
+		//Test sending a vote to the polls.
+		for(int i = 0; i< numberOfPolls; i++)
+		{
+			testClient.vote(String.valueOf(pollIds[i])+ " 3", PollServer.VOTING_PORT);
+		}
+		
+		return testResult;
+	}
+	/**
+	 *Test sending the different admin commands.   
+	 * @param void
+	 * @return false if any of the messages fail
+	 */
+	public boolean adminMessages()
+	{
+		Boolean testResult = true;
+		AdminClient admin = new AdminClient(PollServer.ADMIN_PORT);
+		Client testClient = new Client(PollServer.VOTING_PORT);
+		//Creating Poll
+		int pollId = admin.createPoll("5", "email@domain.ca");
+		if(pollId == 0) testResult = false;	//Will fail if no pollId is returned.
+		
+		//startingPoll
+		admin.startPoll(String.valueOf(pollId));
+		testClient.vote(String.valueOf(pollId) + " 3", PollServer.VOTING_PORT);// Will send a vote to see if the poll has started.
+		
+		//Pausing Poll
+		admin.pausePoll(String.valueOf(pollId));
+		testClient.vote(String.valueOf(pollId)+ " 3", PollServer.VOTING_PORT);// Will send a vote to see if the poll has paused.
+		
+		//Resuming Poll
+		admin.resumePoll(String.valueOf(pollId));
+		testClient.vote(String.valueOf(pollId)+ " 3", PollServer.VOTING_PORT);// Will send a vote to see if the poll has resumed.
+		
+		//Clear Poll
+		admin.clearPoll(String.valueOf(pollId));
+		testClient.vote(String.valueOf(pollId)+ " 3", PollServer.VOTING_PORT);// Will send a vote to see if total votes is only 1.
+		
+		//Stop Poll
+		admin.stopPoll(String.valueOf(pollId));
+		testClient.vote(String.valueOf(pollId)+ " 3", PollServer.VOTING_PORT);// Will send a vote to see if the poll has stopped.
+		
+		return testResult;
+		
+	}
+	/**
+	 *Test connecting multiple voters to the server.   
+	 * @param numberOfVoters
+	 * @return false if any of the connections fail
+	 */
+	public boolean voteConnectionTest(int numberOfVoters)
+	{
+		Boolean testResult = true;
+		Client []clients = new Client[numberOfVoters];
+		for(int i = 0; i < numberOfVoters; i++)
+		{
+			clients[i] = new Client(PollServer.VOTING_PORT);
+		}
+		
+		return testResult;
+		
+	}
+	/**
+	 *Test sending multiple messages to the server one
+	 *after the other.   
+	 * @param numberOfVotes
+	 * @return false if any of the votes fail
+	 */
+	public boolean voteTest(int numberOfVotes)
+	{
+		
+		Boolean testResult = true;
+		Client testClient = new Client(PollServer.VOTING_PORT);
+		
+		for(int i = 0; i <numberOfVotes; i++)
+		{		
+			testClient.vote("!->12345 2", PollServer.VOTING_PORT);
+			
+		}
+		return testResult;
+	}
 	/**
 	 *Test admin connections to server    
 	 * @param numberOfAdmins
@@ -37,9 +163,10 @@ public class PollingTest {
 	public boolean connectionTest(int numberOfAdmins){
 		Boolean testResult = true;
 		PollServer server = new PollServer();
+		
 		AdminClient []admins = new AdminClient[numberOfAdmins];
 		server.startListeners(); //start listening for connections
-
+		
 		//Create Admins and test their connections  
 		for(int i = 0; i<admins.length;i++){
 			if (!(new AdminClient(PollServer.ADMIN_PORT).isConnected())){
@@ -50,68 +177,5 @@ public class PollingTest {
 		return testResult;
 	}
 	
-	
-	public void test1()
-	{
-        
-		//Test Section
-		/*System.out.println("Creating five polls");
-		polls[numOfPolls] = connect("5","mock@mockdomain.ca");
-		numOfPolls++;
-		polls[numOfPolls] = connect("4","mock@mockdomain.ca");
-		numOfPolls++;
-		polls[numOfPolls] = connect("3","mock@mockdomain.ca");
-		numOfPolls++;
-		polls[numOfPolls] = connect("2","mock@mockdomain.ca");
-		numOfPolls++;
-		polls[numOfPolls] = connect("1","mock@mockdomain.ca");
-		numOfPolls++;
-		System.out.println("Printing created polls ID");
-		
-		for(int i = 0; i < numOfPolls; i++)
-		{
-			System.out.println("Poll " + i + " has an ID of " + polls[i]);
-		}*/
-	}
-	
-	public void test2(int port)
-	{
-		/*
-		System.out.println("Creating five polls with 5 different admins");
-		AdminClient myPoll2 = new AdminClient(port);
-		AdminClient myPoll3 = new AdminClient(port);
-		AdminClient myPoll4 = new AdminClient(port);
-		AdminClient myPoll5 = new AdminClient(port);
-		
-		polls[numOfPolls] = connect("1", "email1@e.ca");
-		myPoll2.polls[myPoll2.numOfPolls] = myPoll2.connect("2", "email2@e.ca");
-		myPoll3.polls[myPoll3.numOfPolls] = myPoll3.connect("3", "email3@e.ca");
-		myPoll4.polls[myPoll4.numOfPolls] = myPoll4.connect("4", "email4@e.ca");
-		myPoll5.polls[myPoll5.numOfPolls] = myPoll5.connect("5", "email5@e.ca");
-		
-		System.out.println("Printing created polls ID");
-		
-		System.out.println("Admin 1's poll has an ID of " + polls[numOfPolls]);
-		System.out.println("Admin 2's poll has an ID of " + myPoll2.polls[myPoll2.numOfPolls]);
-		System.out.println("Admin 3's poll has an ID of " + myPoll3.polls[myPoll3.numOfPolls]);
-		System.out.println("Admin 4's poll has an ID of " + myPoll4.polls[myPoll4.numOfPolls]);
-		System.out.println("Admin 5's poll has an ID of " + myPoll5.polls[myPoll5.numOfPolls]);
-		*/
-	}
-	public void test3()
-	{
-		/*System.out.println("Sending all different messages");
-		startPoll(String.valueOf(polls[0]));
-		System.out.println("Sent Start.");
-		pausePoll(String.valueOf(polls[0]));
-		System.out.println("Sent Pause.");
-		resumePoll(String.valueOf(polls[0]));
-		System.out.println("Sent Resume.");
-		clearPoll(String.valueOf(polls[0]));
-		System.out.println("Sent Clear.");
-		stopPoll(String.valueOf(polls[0]));
-		System.out.println("Sent Stop.");
-		*/
-	}
 
 }

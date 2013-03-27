7bb7f888abbac7cac23af99f65ba0502a6d00703
Geoffrey Kinson <gakinson@hotmail.com>
2013-03-26 21:58:11 -0400

    Added another test along with some outputs to a file. Concurrency test shows that some votes do indeed get dropped.

--- src/unitTests/Milestone3Testing.java
+++ src/unitTests/Milestone3Testing.java
@@ -2,6 +2,11 @@ package unitTests;
 
 import static org.junit.Assert.*;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,29 +20,46 @@ import adminGUI.AdminGUI;
 
 
 public class Milestone3Testing {
-
+	
+	static PrintWriter out;
 	int numOfPolls;
+	int go = 0;
 	static PollServer server = new PollServer();
 	static PollsManager manager;
+	/*@After 
+	public void afterClass() throws Exception
+	{
+		
+	}*/
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		server.testingStartListeners();
 		manager = PollsManager.getInstance();
+		out = new PrintWriter(new FileWriter("outputfile.txt", true)); 
 	}
-
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception
+	{
+		out.close();
+	}
 	@Test
 	public final void massVoting() {
-		assertTrue(massVotes(10000));
+		assertTrue(massVotes(1000));
 	}
 	@Test
 	public final void massAdminPolls()
 	{
-		assertTrue(massPolls(5000));
+		assertTrue(massPolls(1000));
 	}
 	@Test
 	public final void massAdmins()
 	{
-		assertTrue(massAdminClients(5000));
+		assertTrue(massAdminClients(1000));
+	}
+	@Test
+	public final void concurrentVotes()
+	{
+		assertTrue(concurVotes(50));
 	}
 	
 
@@ -46,20 +68,27 @@ public class Milestone3Testing {
 	 */
 	public boolean massVotes(int numberOfVotes)
 	{
+		out.println(System.nanoTime() + " -------------Mass Votes Test--------- Voting " + numberOfVotes + " times on one poll.");
+		boolean returnVal;
 		int numVotes = numberOfVotes;
 		long pollID;
 		int votes[] = new int[10];
 		
 		client.AdminClient admin = new client.AdminClient(server.ADMIN_PORT);
 		client.Client voters[] = new client.Client[numberOfVotes];
+		
 		pollID = admin.createPoll("test@test.com|Testing Question|2|Yes|No");
+		
 		System.out.println("Created poll with two options and pollID is: " + pollID);
+		out.println(System.nanoTime() + " - Starting creating voters and vote");
+		
 		for(int i = 0; i < numberOfVotes; i++)
 		{
 			voters[i] = new client.Client(server.VOTING_PORT);
 			voters[i].vote(pollID,1);
 			voters[i].close();
 		}
+		out.println(System.nanoTime() + " - All clients have finished voting, Verifying");
 		System.out.println(numberOfVotes + " clients voted for option 1");
 		votes = manager.getVotes(pollID);
 		System.out.println("Number of votes for option 1: " + votes[0]);
@@ -74,20 +103,30 @@ public class Milestone3Testing {
 		
 		if(votes[0] == numberOfVotes)
 		{
-			return true;
+			out.println(System.nanoTime() + " - Successly voted " + numberOfVotes + " times.");
+			returnVal =true;
 		}
 		else
 		{
+			out.println(System.nanoTime() + " - Fail, all votes did not get through. " + votes[0] + " number of votes got through");
 			System.out.println("Fail");
+			returnVal = false;
 		}
-		return false;
+		
+
+		return returnVal;
 		
 	}
+	/*
+	 * 
+	 */
 	public boolean massPolls(int numberOfPolls)
 	{
+		out.println(System.nanoTime() + " -------------Mass Polls Test---------Creating " + numberOfPolls + " for one admin.");
 		AdminClient admin = new AdminClient(server.ADMIN_PORT);
 		long pollID;
 		int numOfCreatedPolls = 0;
+		out.println(System.nanoTime() + " - Starting creating polls");
 		for(int i = 0; i<numberOfPolls;i++)
 		{
 			pollID = admin.createPoll("test@test.com|poll" + i +"|2|yes|no");
@@ -97,28 +136,115 @@ public class Milestone3Testing {
 			}
 			
 		}
+		out.println(System.nanoTime() + " - Finished creating polls, verifying");
 		if(numOfCreatedPolls == numberOfPolls)
 		{
+			out.println(System.nanoTime() + " - Successly created " + numberOfPolls + " polls for the admin client.");
 			return true;
 		}
+		out.println(System.nanoTime() + " - Fail, not all polls were created and stayed active for the admin. " + numOfCreatedPolls + " polls were created.");
 		return false;
 	}
+	
+	/*
+	 * 
+	 */
 	public boolean massAdminClients(int numberOfAdmins)
 	{
-		
+		boolean returnVal = true;
 		AdminClient admins[] = new AdminClient[numberOfAdmins];
+		
+		out.println(System.nanoTime() + " -------------Mass Votes Test---------Creating " + numberOfAdmins + " Admin clients on the server.");
+		out.println(System.nanoTime() + " - Starting creating Admins");
+		
 		for(int i = 0;i<numberOfAdmins;i++)
 		{
 			admins[i] = new AdminClient(server.ADMIN_PORT);
 			
 		}
+		out.println(System.nanoTime() + " - Finished creating Admins, Verifying");
 		for(int i = 0; i < numberOfAdmins;i++)
 		{
 			if(admins[i].isConnected() == false)
 			{
-				return false;
+				out.println(System.nanoTime() + " - Fail, not all admins were created and stay connected to the server.");
+				returnVal = false;
 			}
 		}
-			return true;
+		if(returnVal != false)
+		{
+			out.println(System.nanoTime() + " - Successly created " + numberOfAdmins + " Admin clients.");
+			returnVal = true;
+	
+		}
+		for(int i = 0; i<numberOfAdmins;i++)
+		{
+			admins[i].disconnect();
+		}
+		return returnVal;
+	}
+	
+	public boolean concurVotes(int numberOfVotes)
+	{
+		boolean returnVal = true;
+		long pollID;
+		int votes[] = new int[10];
+		voter voting[] = new voter[numberOfVotes];
+		AdminClient admin = new AdminClient(server.ADMIN_PORT);
+		
+		pollID = admin.createPoll("test@test.com|poll|2|yes|no");
+		
+		out.println("Setting up voters");
+		for(int i = 0; i < numberOfVotes;i++)
+		{
+			voting[i] = new voter(this,pollID);
+			voting[i].start();
+		}
+		out.println("Voters setup");
+		go = 1;
+		out.println("voters started voting");
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		out.println("voters voted");
+		votes = manager.getVotes(pollID);
+		if(votes[0] == numberOfVotes)
+		{
+			out.println("Success");
+			returnVal = true;
+		}
+		else
+		{
+			out.println("Fail " + votes[0]);
+			returnVal = false;
+		}
+		
+		
+		return returnVal;
+		
+		
+	}
+}
+class voter extends Thread
+{
+	Milestone3Testing global;
+	Client voter;
+	long pollID;
+	voter(Milestone3Testing global, long pollID)
+	{
+		this.global = global;
+		voter = new Client(global.server.VOTING_PORT);
+		this.pollID = pollID;
+	}
+	
+	public void run()
+	{
+		while(global.go == 0)
+		{}
+		voter.vote(pollID, 1);
+		
 	}
 }
